4876c6f89d84fab5b7001bb389abdfdbfd8d0002
HYEIN-SEP <hyeinpark22@gmail.com>
2023-07-31 02:23:19 +0900

    추가:html,css,js 작성

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $(".b_slides").bxSlider({
+    slideWidth: 480,
+    minSlides: 4,
+    maxSlides: 5,
+    moveSlides: 1,
+    slideMargin: 0,
+    pager: false,
+    controls: false,
+  });
+});
