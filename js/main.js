0c484e982e553730d743e3fc328edf3cee55b17d
triallife <nanamutok@gmail.com>
2023-07-31 16:09:25 +0900

    [0731] 추가,삭제 : main header 기본형 완성, svg black 파일 삭제

--- js/main.js
+++ js/main.js
@@ -1,14 +1,58 @@
-let mainMenu = document.querySelectorAll('.main_menu'),
-    twodebth = document.querySelectorAll('.main_menu li ul > li'),
-		header = document.querySelector('header');
-        console.log(twodebth);
-mainMenu.forEach(item=>{
-	item.addEventListener('mouseover',()=>{
-		header.style.height = '600px';
-        // headerAfer.style.opacity = 1;
-        header.style.backgroundColor = 'rgba(0,0,0,.7)';
+let mainMenu = $('.main_menu > li'),
+	twodebth = $('.main_menu li ul'),
+	header = $('header'),
+	formLabel = $('.search_bar label'),
+	link = $('.main_menu li a');
+
+link.hover(function () {
+	$(this).css({ color: 'var(--BC-LGRed)' })
+	},	
+	function () {
+	$(this).css({ color:'var(--MC-LGWhite)'})
+ });
+mainMenu.hover(
+	function () {
+		header.css({
+			height: '600px',
+			backgroundColor: 'rgba(0,0,0,.7)'
+		}),
+		twodebth.css({
+			display: 'flex'
+		}),
+			$('.line').css({
+			opacity: 1
+		})
+		// header.html('<span></span>');
+	},
+	function () {
+		header.css({
+			height: '150px',
+			backgroundColor: 'transparent'
+		}),
+		twodebth.css({
+			display: 'none'
+		}),
+		$('.line').css({
+			opacity: 0
+		})
+	}
+);
+
+formLabel.mouseenter(function () {
+	header.css({
+		height: '150px',
+		backgroundColor: 'transparent'
+	}),
+	twodebth.css({
+		display: 'none'
+	}),
+	$('.main_menu').css({
+		display: 'none'
 	});
-	item.addEventListener('mouseout',()=>{
-		header.style.height = '150px';
+});
+
+$('form').mouseleave(function () {
+	$('.main_menu').css({
+		display: 'flex'
 	});
 })
