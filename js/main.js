96f6991d6f96c0685d55b72a6e262ecab14a4521
jeongewon <2wonyee@gmail.com>
2023-08-01 20:18:29 +0900

    [0801] html, css 추가 및 수정

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
-$( function() {
+$(function() {
     $( ".news_box" ).accordion({
       heightStyle: "content"
     });
-  } );
+
+    let newsBtn = $('.news_box').find('button');
+    newsBtn.click(function(){
+      
+    })
+
+    
+  });
