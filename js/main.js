ab7053e274d1db00f72416e1e94d3c5b714e838e
triallife <nanamutok@gmail.com>
2023-08-01 18:21:54 +0900

    [0801] 추가 : aside css 위치 조정

--- js/main.js
+++ js/main.js
@@ -5,106 +5,120 @@ let mainMenu = $('.main_menu > li'),
   headerClone = header.contents().clone(),
   headerCloneContainer = $('<div class="header-clone"></div>');
   
+
+if ($(window).width() > 768) {
   headerClone.appendTo(headerCloneContainer);
   $('body').append(headerCloneContainer);
-
-$(window).scroll(function(){
-  if($(this).scrollTop() > 0){
-    headerCloneContainer.addClass('visible');
-    header.hide();
-  }else{
-    headerCloneContainer.removeClass('visible');
-    header.show();
-  }
-});
-
+  $(window).scroll(function(){
+    if($(this).scrollTop() > 0){
+      headerCloneContainer.addClass('visible');
+      header.hide();
+    }else{
+      headerCloneContainer.removeClass('visible');
+      header.show();
+    }
+  });
+  
 let clone = $('.header-clone'),
-    c_mainmenu = clone.find('.main_menu > li'),
-    c_twodebth = clone.find('.main_menu li ul');
-    console.log(c_twodebth);
+c_mainmenu = clone.find('.main_menu > li'),
+c_twodebth = clone.find('.main_menu li ul');
+console.log(c_twodebth);
 
 c_mainmenu.hover(function(){
   clone.css({
-    paddingBottom: '54px'
+  paddingBottom: '54px'
   }),
   c_twodebth.css({
-    display: 'flex'
+  display: 'flex'
   }),
   $('.line').css({
-    opacity: 1
+  opacity: 1
   })
-},function(){
+  },function(){
   clone.css({
-    paddingBottom: '47px'
+  paddingBottom: '47px'
   }),
   c_twodebth.css({
-    display: 'none'
+  display: 'none'
   }),
   $('.line').css({
-    opacity: 0
+  opacity: 0
   })
 })
 
 mainMenu.hover(
-	function () {
-		header.css({
-			height: '600px',
-			backgroundColor: 'rgba(0,0,0,.7)'
-		}),
-		twodebth.css({
-			display: 'flex'
-		}),
-			$('.line').css({
-			opacity: 1
-		})
-	},
-	function () {
-		header.css({
-			height: '150px',
-			backgroundColor: 'transparent'
-		}),
-		twodebth.css({
-			display: 'none'
-		}),
-		$('.line').css({
-			opacity: 0
-		})
-	}
+function () {
+header.css({
+  height: '600px',
+  backgroundColor: 'rgba(0,0,0,.7)'
+}),
+twodebth.css({
+  display: 'flex'
+}),
+  $('.line').css({
+  opacity: 1
+})
+},
+function () {
+header.css({
+  height: '150px',
+  backgroundColor: 'transparent'
+}),
+twodebth.css({
+  display: 'none'
+}),
+$('.line').css({
+  opacity: 0
+})
+}
 );
 
-console.log(clone.find('.main_menu'));
-
 $('label').mouseenter(function () {
-  if(clone.hasClass('visible')){
-    clone.find('.menu .main_menu').css({
-      visibility: 'hidden'
-    })
-  }else{
-    $('.menu .main_menu').css({
-      visibility: 'hidden'
-    })
-  }
+if(clone.hasClass('visible')){
+clone.find('.menu .main_menu').css({
+  visibility: 'hidden'
+})
+}else{
+$('.menu .main_menu').css({
+  visibility: 'hidden'
+})
+}
 });
 
 $('form').mouseleave(function () {
-  if(clone.hasClass('visible')){
-    clone.find('.main_menu').css({
-      visibility: 'visible'
-    })
-  } else {
-    $('.main_menu').css({
-      visibility: 'visible',
-    })
-  }
+if(clone.hasClass('visible')){
+clone.find('.main_menu').css({
+  visibility: 'visible'
+})
+} else {
+$('.main_menu').css({
+  visibility: 'visible',
+})
+}
 })
 
 $('.lang').mouseenter(function(){
-  $('.lang_list').css({
-    display: 'block'
-  })
+$('.lang_list').css({
+display: 'block'
+})
 });
 $('.lang_list').mouseleave(function(){
-  $('.lang_list').css({
-    display: 'none'
-  })
+$('.lang_list').css({
+display: 'none'
 })
+})
+}//desktop
+// if ($(window).width() > 480) { //tablet
+
+// } else { //mobile
+  
+// }
+
+let title = $('.title a'),
+  info = $('.info');
+    
+title.on('click', function (e) {
+  e.preventDefault();
+  $(this).parent().siblings('li').find('> ul').slideUp();
+  $(this).parent().find('> ul').slideToggle();
+  })
