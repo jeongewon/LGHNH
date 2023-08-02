d11a536298a369adb7f425ed4e667875c7ced8b4
triallife <nanamutok@gmail.com>
2023-08-02 17:31:56 +0900

    [0802] 수정 : header 코드 정리

--- js/main.js
+++ js/main.js
@@ -1,16 +1,21 @@
+/* 이은서 nav */
 let mainMenu = $('.origin .main_menu > li'),
-	twodebth = $('.main_menu li ul'),
-	formLabel = $('.search_bar label'),
-  header = $('header'),
-  headerClone = $('.origin').contents().clone(),
-  headerCloneContainer = $('<div class="header-clone"></div>'),
-  width = $(window).width(),
-  // window = $(window),
-  close_aside = $('.close_btn'),
-  open_aside = $('.aside_btn');
-  console.log(width);
+    twodebth = $('.main_menu li ul'),
+    formLabel = $('.search_bar label'),
+    header = $('header'),
+    headerClone = $('.origin').contents().clone(),
+    headerCloneContainer = $('<div class="header-clone"></div>'),
+    width = $(window).width(),
+    close_aside = $('.close_btn'),
+    open_aside = $('.aside_btn'),
+    title = $('.title a');
+
 headerClone.appendTo(headerCloneContainer);
 $('body').append(headerCloneContainer);
+
+let clone = $('.header-clone'),
+    c_mainmenu = clone.find('.main_menu > li'),
+    c_twodebth = clone.find('.main_menu li ul');
 
 $(window).scroll(function () {
   width = $(window).width();
@@ -31,132 +36,98 @@ $(window).scroll(function () {
     }
   }
 })
-  
-let clone = $('.header-clone'),
-c_mainmenu = clone.find('.main_menu > li'),
-c_twodebth = clone.find('.main_menu li ul');
-
-c_mainmenu.hover(function(){
-  let width = $(window).width();
-  if (width  > 768) {
-    clone.css({
-      paddingBottom: '54px'
-      }),
-      c_twodebth.css({
-      display: 'flex'
-      }),
-      $('.line').css({
-      opacity: 1
-      })
-  }
-  },function(){
-    let width = $(window).width();
-    if (width > 768) {
-      clone.css({
-        paddingBottom: '47px'
-        }),
-        c_twodebth.css({
-        display: 'none'
-        }),
-        $('.line').css({
-        opacity: 0
-        })
-    }
-})
 
 mainMenu.hover(function () {
-    $('.origin').css({
-      height: '600px',
-      backgroundColor: 'rgba(0,0,0,.7)'
-    }),
-    twodebth.css({
-      display: 'flex'
-    }),
-      $('.line').css({
-      opacity: 1
-    })
-  }, function () {
-    $('.origin').css({
-      height: '150px',
-      backgroundColor: 'transparent'
-    }),
-    twodebth.css({
-      display: 'none'
-    }),
+  $('.origin').css({
+    height: '600px',
+    backgroundColor: 'rgba(0,0,0,.7)'
+  }),
+  twodebth.css({
+    display: 'flex'
+  }),
     $('.line').css({
-      opacity: 0
-    })
+    opacity: 1
+  })
+}, function () {
+  $('.origin').css({
+    height: '150px',
+    backgroundColor: 'transparent'
+  }),
+  twodebth.css({
+    display: 'none'
+  }),
+  $('.line').css({
+    opacity: 0
+  })
+});
+c_mainmenu.hover(function () {
+  let width = $(window).width();
+  if (width > 768) {
+    clone.css({
+      paddingBottom: '54px'
+    }),
+      c_twodebth.css({
+        display: 'flex'
+      }),
+      $('.line').css({
+        opacity: 1
+      })
   }
-);
-
-$('label').mouseenter(function () {
-if(clone.hasClass('visible')){
-clone.find('.menu .main_menu').css({
-  visibility: 'hidden'
-})
-}else{
-$('.menu .main_menu').css({
-  visibility: 'hidden'
-})
-}
+}, function () {
+  let width = $(window).width();
+  if (width > 768) {
+    clone.css({
+      paddingBottom: '47px'
+    }),
+      c_twodebth.css({
+        display: 'none'
+      }),
+      $('.line').css({
+        opacity: 0
+      })
+  }
 });
 
+$('label').mouseenter(function () {
+  if(clone.hasClass('visible')){
+    clone.find('.menu .main_menu').css({
+      visibility: 'hidden'
+    })
+  }else{
+    $('.menu .main_menu').css({
+      visibility: 'hidden'
+    })
+  }
+});
 $('form').mouseleave(function () {
-if(clone.hasClass('visible')){
-clone.find('.main_menu').css({
-  visibility: 'visible'
+  if(clone.hasClass('visible')){
+    clone.find('.main_menu').css({
+      visibility: 'visible'
+  })
+  } else {
+    $('.main_menu').css({
+      visibility: 'visible',
+    })
+  }
 })
-} else {
-$('.main_menu').css({
-  visibility: 'visible',
-})
-}
-})
-
 $('.lang').mouseenter(function(){
-  let width = $(window).width();
-  if (width  > 768) {
     $('.lang_list').css({
       display: 'block'
       })
-  }else{
-    $('.lang_list').css({
-      display: 'flex'
-      })
-  }
 });
 $('.lang_list').mouseleave(function(){
-  let width = $(window).width();
-  if (width  > 768) {
     $('.lang_list').css({
       display: 'none'
       })
-  }else{
-    $('.lang_list').css({
-      display: 'flex'
-      })
-  }
 })
 
-
-// if ($(window).width() > 480) { //tablet
-
-// } else { //mobile
-  
-// }
-
-let title = $('.title a'),
-  info = $('.info');
-    
+/* aside */
 title.on('click', function (e) {
   e.preventDefault();
   $(this).parent().siblings('li').find('> ul').slideUp();
   $(this).parent().find('> ul').slideToggle();
   $(this).toggleClass('active');
-  // $(this).parent().siblings('li').find('> a').removeClass('active');
-  // $(this).find('span').toggleClass('active');
   })
-
 open_aside.click(function(){
   $('.aside_menu_side').css({right: '0'})
 })
@@ -164,8 +135,7 @@ close_aside.click(function(){
   $('.aside_menu_side').css({right: '-286px'})
     open_aside.show();
 });
-
-
+/* resize */
 $(window).resize(function(){
   width = $(window).width();
   console.log(width);
