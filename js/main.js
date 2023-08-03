0716ca8720e95c8590b79534c96adc32934e9012
HYEIN-SEP <hyeinpark22@gmail.com>
2023-08-03 15:18:18 +0900

    [0803]추가:html,css,js,images,slickjs 추가

--- js/main.js
+++ js/main.js
@@ -1,11 +1,60 @@
-$(document).ready(function () {
-  $(".b_slides").bxSlider({
-    slideWidth: 480,
-    minSlides: 4,
-    maxSlides: 5,
-    moveSlides: 1,
-    slideMargin: 0,
-    pager: false,
-    controls: false,
-  });
+const multipleSwiper = new Swiper(".multiple-slide", {
+  slidesPerView: 4,
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    pauseOnMouseEnter: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 2,
+    },
+    481: {
+      slidesPerView: 3,
+    },
+    769: {
+      slidesPerView: 4,
+    },
+  },
+});
+
+const bMenuSwiper = new Swiper(".b-menu-slide", {
+  slidesPerView: 3,
+  slidesToScroll: 1,
+  loop: true,
+  navigation: {
+    nextEl: ".control-next",
+    prevEl: ".control-prev",
+  },
+});
+
+bMenuSwiper.on("realIndexChange", function (a) {
+  let idx = (a.realIndex % 3) + 1;
+  console.log(idx);
+  $(".slidetext-wrapper").hide();
+  $("#business-" + idx).show();
+});
+
+$(".new_slides").slick({
+  centerMode: true,
+  centerPadding: 60,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  arrows: true,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
 });
