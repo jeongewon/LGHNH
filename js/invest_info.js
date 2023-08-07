df14433d668d2b4c75ad74a99e70b28102fccdd6
HYEIN-SEP <hyeinpark22@gmail.com>
2023-08-07 16:35:05 +0900

    [0807]추가: 주식api, rnd.css 추가

--- js/invest_info.js
+++ js/invest_info.js
@@ -18,3 +18,21 @@ alloTab.click(function () {
   inveTable2.hide();
   $("#al" + idx).show();
 });
+
+
+ // $.getJSON('경로', 할일);
+ $.getJSON('https://apis.data.go.kr/1160100/service/GetStockSecuritiesInfoService/getStockPriceInfo?serviceKey=oZsqCqu3rv9%2Frp1M6cXfKdEN%2Fpyf02cYdsBynYdUCzHV8lbaV8yxaqtTdCqXshKxu2wwUaoIZ0%2FU2uO0cFIu5Q%3D%3D&resultType=json&itmsNm=LG%EC%83%9D%ED%99%9C%EA%B1%B4%EA%B0%95', function(data){
+  console.log(data);
+  console.log(data.response.body.items.item[0].mkp)
+
+  let Dhdl = data.response.body.items.item[0];
+
+  $('.dhdl h1').append(Dhdl.clpr);
+  $('.dhdl').append(Dhdl.vs);
+  $('.dldl').append(Dhdl.hipr);
+  $('.dhdh').append(Dhdl.trqu);
+  $('.dkdk').append(Dhdl.lopr);
+  $('.tntn').append(Dhdl.mrktTotAmt/Dhdl.lstgStCnt);
+
+
+});
