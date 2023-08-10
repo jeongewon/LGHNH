2413ed046805f41f3dbab46bf80b91f8f65f5665
SOOSLOANE <soosloane@gmail.com>
2023-08-10 09:56:56 +0900

    [0810] beauty 페이지 html, css, js 수정

--- js/beauty.js
+++ js/beauty.js
@@ -1,126 +1,65 @@
-/* Header */
+/* ********************** Header ********************** */
 let mainMenu = $('.origin .main_menu > li'),
-    twodebth = $('.main_menu li ul'),
-    formLabel = $('.search_bar label'),
-    header = $('header'),
-    headerClone = $('.origin').contents().clone(),
-    headerCloneContainer = $('<div class="header-clone"></div>'),
-    width = $(window).width(),
-    close_aside = $('.close_btn'),
-    open_aside = $('.aside_btn'),
-    title = $('.title > a');
+	twodebth = $('.main_menu li ul'),
+	formLabel = $('.search_bar label'),
+  header = $('.origin'),
+  aside_header = $('.aside'),
+  width = $(window).width(),
+  close_aside = $('.close_btn'),
+  open_aside = $('.aside_btn'),
+  clone = $('.header-clone'),
+  c_mainmenu = clone.find('.main_menu > li'),
+  c_twodebth = clone.find('.main_menu li ul'),
+  title = $('.title > a'),
+  info = $('.info');
 
-headerClone.appendTo(headerCloneContainer);
-$('body').append(headerCloneContainer);
-
-let clone = $('.header-clone'),
-    c_mainmenu = clone.find('.main_menu > li'),
-    c_twodebth = clone.find('.main_menu li ul');
-
-$(window).scroll(function () {
-  width = $(window).width();
-  if (width > 768) {
-    if ($(this).scrollTop() > 0) {
-      $('.origin').hide();
-      headerCloneContainer.addClass('visible');
-      // $('.header-clone.visible .desktop_menu').show();
-    } else {
-      headerCloneContainer.removeClass('visible');
-      $('.origin').show();
-    }
-  } else {
-    if ($(this).scrollTop() > 0) {
-      header.addClass('white');
-    } else {
-      header.removeClass('white');
-    }
-  }
-})
-
-mainMenu.hover(function () {
-  $('.origin').css({
-    height: '600px',
-    backgroundColor: 'rgba(0,0,0,.7)'
-  }),
-  twodebth.css({
-    display: 'flex'
-  }),
-    $('.line').css({
-    opacity: 1
-  })
-}, function () {
-  $('.origin').css({
-    height: '150px',
-    backgroundColor: 'transparent'
-  }),
-  twodebth.css({
-    display: 'none'
-  }),
-  $('.line').css({
-    opacity: 0
-  })
-});
-c_mainmenu.hover(function () {
-  let width = $(window).width();
-  if (width > 768) {
+c_mainmenu.hover(function(){
     clone.css({
       paddingBottom: '54px'
-    }),
+      }),
       c_twodebth.css({
-        display: 'flex'
+      display: 'flex'
       }),
       $('.line').css({
-        opacity: 1
+      opacity: 1
       })
-  }
-}, function () {
-  let width = $(window).width();
-  if (width > 768) {
-    $('.line').css({
-      opacity: 0
-    }),
-    clone.css({
-      paddingBottom: '47px'
-    }),
-    c_twodebth.css({
-      display: 'none'
-    })
-  }
-});
-
+  },function(){
+      clone.css({
+        paddingBottom: '47px'
+        }),
+        c_twodebth.css({
+        display: 'none'
+        }),
+        $('.line').css({
+        opacity: 0
+        })
+})
 $('label').mouseenter(function () {
   if(clone.hasClass('visible')){
     clone.find('.menu .main_menu').css({
       visibility: 'hidden'
-    })
-  }else{
-    $('.menu .main_menu').css({
-      visibility: 'hidden'
-    })
+  })
+} else {
+  $('.menu .main_menu').css({
+    visibility: 'hidden'
+  })
   }
 });
 $('form').mouseleave(function () {
-  if(clone.hasClass('visible')){
-    clone.find('.main_menu').css({
-      visibility: 'visible'
-  })
-  } else {
-    $('.main_menu').css({
-      visibility: 'visible',
-    })
-  }
+clone.find('.main_menu').css({
+  visibility: 'visible'
+})
 })
 $('.lang').mouseenter(function(){
-    $('.lang_list').css({
-      display: 'block'
-      })
+  $('.lang_list').css({
+    display: 'block'
+    })
 });
 $('.lang_list').mouseleave(function(){
-    $('.lang_list').css({
-      display: 'none'
-      })
+  $('.lang_list').css({
+    display: 'none'
+    })
 })
-
 /* aside */
 title.on('click', function (e) {
   e.preventDefault();
@@ -142,31 +81,41 @@ close_aside.click(function(){
   $(this).removeClass('active');
   title.parent().find('> ul').slideUp();
 });
-
 /* resize */
 $(window).resize(function(){
   width = $(window).width();
   if (width > 768) {
-    if ($(window).scrollTop() > 0) {
-      headerCloneContainer.addClass('visible')
-    } else {
-      $('.origin').show();
-    }
+    header.show();
   } else {
-    if (headerCloneContainer.hasClass('visible')) {
-      headerCloneContainer.removeClass('visible');
-    }
+    header.hide();
+    $('.aside_menu_side').css({right: '-286px'})
   }
 })
 
-/* Beauty */
+/* ********************** Beauty ********************** */
+$(window).on('scroll', function(){
+  console.log($(window).scrollTop());
+})
+
+let winSCT = $(window).scrollTop(),
+    introDV = $('.beauty_lineup3'),
+    introDV_SCT = 250;
+
+$(window).scroll(function(){
+  let window_SCT = $(this).scrollTop(),
+      introDivision = $('.beauty_page1 .beauty_division'),
+      introDivision_SCT = 400;
+ 
+  if(window_SCT < introDivision_SCT){
+    introDivision.stop().animate({width: '95%'}, {duration: 400});
+  }else {
+    introDivision.stop().animate({width: '85%'}, {duration: 400}); 
+  }
+})
+
+AOS.init();
+
 $(function(){  
-
-  let intro_h3 = $('.beauty_intro h3'),  //사업분야
-      intro_h2 = $('.beauty_intro h2'),  //글로벌 명품 BEAUTY
-      intro_p = $('.beauty_intro p'),  
-      introDivision = $('.beauty_division');
-
   //Beauty S3 Swiper Slide
   const beautySwiper = new Swiper('.beauty_s3_multiple', {
     slidesPerView: 5,
@@ -205,25 +154,23 @@ $(function(){
 
 
 
-/*
-$(window).on('scroll', function(){
-  $(window).scrollTop();
-})
-*/
 
-/* line-up 페이드인 애니메이션
-$(window).on('scroll', function(){
-  let windowScrollNum = $(window).scrollTop();
-  let bizCate = $('.beauty_intro h3');
-  let introTitle = $('.beauty_intro h2');
-  let introDesc = $('.beauty_intro p');
-  let division1 = $('.beauty_intro + .beauty_division');
-  
-  if(windowScrollNum >= 0){
-    bizCate.addClass('beauty_lineup');
-    introTitle.addClass('beauty_lineup');
-    introDesc.addClass('beauty_lineup');
-    division1.addClass('beauty_lineup');
+
+/* ********************** Aside ********************** */
+let goTop = $('#top');
+
+$(window).on('scroll', function () {
+  let tct = $(this).scrollTop();
+  if (tct > 500) {
+    goTop.addClass('active');
+  } else {
+    goTop.removeClass('active');
   }
+})
+
+goTop.click(function (e) {
+  e.preventDefault();
+  $('html,body').stop().animate({
+    scrollTop: 0
+  }, 'easeInCubic');
 });
-*/
